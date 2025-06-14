67c5075679b8df059f500262ef48dadab6bd96de
T4rp <why9ten@gmail.com>
2025-06-14 15:41:06 -0400

    feat: implement fast fourier transform

--- src/fft.rs
+++ src/fft.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, AddAssign};
+use std::{
+    f32::consts::PI,
+    ops::{Add, AddAssign, Mul, MulAssign},
+};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Complex {
@@ -29,10 +32,123 @@ impl AddAssign for Complex {
     }
 }
 
+impl Mul for Complex {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Complex {
+            re: self.re * rhs.re - self.im * rhs.im,
+            im: self.re * rhs.im + self.im * rhs.re,
+        }
+    }
+}
+
+impl MulAssign for Complex {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
 pub fn cnum(re: f32, im: f32) -> Complex {
     Complex::new(re, im)
 }
 
 pub fn exp_im(im: f32) -> Complex {
     Complex::new(im.cos(), im.sin())
+}
+
+pub fn ditfft2(x: &[Complex], n: usize, s: usize) -> Vec<Complex> {
+    if n == 1 {
+        return vec![x[0]];
+    }
+
+    let even = ditfft2(x, n / 2, s * 2);
+    let odd = ditfft2(&x[s..], n / 2, s * 2);
+
+    let mut output = vec![Complex::new(0.0, 0.0); n];
+    for k in 0..(n / 2) {
+        let q = exp_im(-2.0 * PI * k as f32 * n as f32) * odd[k];
+        output[k] = even[k] + q;
+        output[k + n / 2] = even[k] + (q * Complex::new(-1.0, 0.0))
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use plotters::prelude::*;
+
+    #[test]
+    fn runs_normally() {
+        let input = vec![0.0, 1.0, 3.0, 4.0];
+        let cinput: Vec<Complex> = input.iter().map(|n| Complex::new(*n, 0.0)).collect();
+        std::hint::black_box(ditfft2(&cinput, cinput.len(), 1));
+    }
+
+    #[test]
+    fn run_and_plot() {
+        let input: Vec<f32> = (0..300)
+            .into_iter()
+            .map(|x| (x as f32 / 20.0 * 2.0).sin())
+            .collect();
+
+        let cinput: Vec<Complex> = input.iter().map(|n| Complex::new(*n, 0.0)).collect();
+        let out = ditfft2(&cinput, cinput.len(), 1);
+
+        let mut last_largest = 0.0;
+        let mut max_index_at = 0;
+
+        for (i, x) in out.iter().enumerate() {
+            if x.re > last_largest {
+                last_largest = x.re;
+                max_index_at = i
+            }
+        }
+
+        let root = BitMapBackend::new("coolio.png", (1024, 768)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+
+        root.draw_text(
+            format!("Largest: {}", max_index_at as f32 / 20.0).as_str(),
+            &("sans-serif", 24).into_text_style(&root),
+            (0, 0),
+        );
+
+        let mut chart = ChartBuilder::on(&root)
+            .x_label_area_size(35)
+            .y_label_area_size(40)
+            .right_y_label_area_size(40)
+            .margin(5)
+            .caption("FFT plot", ("sans-serif", 50.0).into_font())
+            .build_cartesian_2d(0f32..300.0, (-2.0f32..2.0f32))
+            .unwrap();
+
+        chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .disable_y_mesh()
+            .y_label_formatter(&|x| format!("{:e}", x))
+            .draw()
+            .unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                input.iter().enumerate().map(|(i, x)| return (i as f32, *x)),
+                &BLUE,
+            ))
+            .unwrap()
+            .label("Original");
+
+        chart
+            .draw_series(LineSeries::new(
+                out.iter().enumerate().map(|(i, x)| return (i as f32, x.re)),
+                &RED,
+            ))
+            .unwrap()
+            .label("Fourier transformed");
+
+        root.present().unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod fft;
-
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::{thread, time::Duration};
 
@@ -31,4 +29,64 @@ fn main() {
 
     stream.play().unwrap();
     thread::sleep(Duration::from_secs(10));
+}
+
+#[cfg(test)]
+mod tests {
+    use plotters::prelude::*;
+
+    #[test]
+    fn plot() {
+        let root = BitMapBackend::new("test.png", (1024, 768)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+
+        let mut chart = ChartBuilder::on(&root)
+            .x_label_area_size(35)
+            .y_label_area_size(40)
+            .right_y_label_area_size(40)
+            .margin(5)
+            .caption("Dual Y-Axis Example", ("sans-serif", 50.0).into_font())
+            .build_cartesian_2d(0f32..10f32, (0.1f32..1e10f32).log_scale())
+            .unwrap()
+            .set_secondary_coord(0f32..10f32, -1.0f32..1.0f32);
+
+        chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .disable_y_mesh()
+            .y_desc("Log Scale")
+            .y_label_formatter(&|x| format!("{:e}", x))
+            .draw()
+            .unwrap();
+
+        chart
+            .configure_secondary_axes()
+            .y_desc("Linear Scale")
+            .draw()
+            .unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                (0..=100).map(|x| (x as f32 / 10.0, (1.02f32).powf(x as f32 * x as f32 / 10.0))),
+                &BLUE,
+            ))
+            .unwrap()
+            .label("y = 1.02^x^2")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
+
+        chart
+            .draw_secondary_series(LineSeries::new(
+                (0..=100).map(|x| (x as f32 / 10.0, (x as f32 / 5.0).sin())),
+                &RED,
+            ))
+            .unwrap()
+            .label("y = sin(2x)")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
+
+        chart
+            .configure_series_labels()
+            .background_style(RGBColor(128, 128, 128))
+            .draw()
+            .unwrap();
+    }
 }
