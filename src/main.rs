81d206068be20ec18f9869f5204ef5f2e63f9b44
T4rp <why9ten@gmail.com>
2025-06-25 06:38:54 -0400

    feat: Add poor note detection + sound plotting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,98 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use core::f32;
+use cpal::{
+    OutputCallbackInfo,
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+};
 use guitartuning::fft::{Complex, ditfft2, hann_window, hann_window_cpx};
-use std::{thread, time::Duration};
+use std::{fs, thread, time::Duration};
 
 const FFT_SIZE: usize = 4096;
+
+const NOTE_FREQ: &[(&str, f32)] = &[
+    ("E2", 82.41),
+    ("A2", 110.0),
+    ("D3", 146.83),
+    ("G3", 196.0),
+    ("E4", 329.63),
+];
+
+const FREQ_THRESH_HOLD: f32 = 50.0;
+
+fn closest_note(freq: f32) -> Option<&'static str> {
+    let mut closest_dist = f32::MAX;
+    let mut detected_note: Option<&'static str> = None;
+
+    for (note, f) in NOTE_FREQ.iter() {
+        let diff = (*f - freq).abs();
+
+        if diff >= FREQ_THRESH_HOLD {
+            continue;
+        }
+
+        if diff < closest_dist {
+            detected_note = Some(note);
+            closest_dist = diff;
+        }
+    }
+
+    detected_note
+}
+
+fn plot_sound(note: &'static str, input: &[Complex], output: &[Complex]) {
+    use plotters::prelude::*;
+
+    let root = BitMapBackend::new("soundpre.png", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    root.draw_text(
+        format!("note: {}", note).as_str(),
+        &("sans-serif", 24).into_text_style(&root),
+        (0, 0),
+    )
+    .unwrap();
+
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(35)
+        .y_label_area_size(40)
+        .right_y_label_area_size(40)
+        .margin(5)
+        .caption("Sound chart", ("sans-serif", 50.0).into_font())
+        .build_cartesian_2d(0f32..(input.len() as f32), (-50.0f32..50.0f32))
+        .unwrap();
+
+    chart
+        .configure_mesh()
+        .disable_x_mesh()
+        .disable_y_mesh()
+        .y_label_formatter(&|x| format!("{:e}", x))
+        .draw()
+        .unwrap();
+
+    chart
+        .draw_series(LineSeries::new(
+            input
+                .iter()
+                .enumerate()
+                .map(|(i, x)| return (i as f32, x.re * 50.0)),
+            &BLUE,
+        ))
+        .unwrap()
+        .label("Original");
+
+    chart
+        .draw_series(LineSeries::new(
+            output
+                .iter()
+                .enumerate()
+                .map(|(i, x)| return (i as f32, x.re)),
+            &RED,
+        ))
+        .unwrap()
+        .label("Fourier transformed");
+
+    root.present().unwrap();
+    fs::copy("soundpre.png", "sound.png").unwrap();
+}
 
 fn main() {
     let host = cpal::default_host();
@@ -43,6 +133,8 @@ fn main() {
                     }
                 }
 
+                let magnitude = output[max_index_at].re;
+
                 let max_freq = max_index_at as f32 * sample_rate as f32 / input.len() as f32;
 
                 let freq = if max_index_at == 0 || max_index_at == output.len() {
@@ -56,7 +148,12 @@ fn main() {
                     max_freq + 0.5 * (alpha - gamma) / (alpha - 2.0 * beta + gamma)
                 };
 
-                println!("{} hz", freq);
+                // if let Some(note) = closest_note(freq) {
+                //     if magnitude > 25.0 {
+                println!("note: {}, {} hz, mag: {}", "", freq, magnitude);
+                plot_sound("", &input, &output);
+                // }
+                // }
             },
             move |err| {
                 eprintln!("{}", err);
@@ -66,7 +163,7 @@ fn main() {
         .unwrap();
 
     stream.play().unwrap();
-    thread::sleep(Duration::from_secs(10));
+    thread::sleep(Duration::from_secs(100));
 }
 
 #[cfg(test)]
